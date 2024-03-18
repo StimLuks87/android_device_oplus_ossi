36a59509e38d315b08e8bd44b94818749d48a183
Stim_Luks <san10031987san@gmail.com>
2024-03-18 23:10:22 +0500

    first commit

---
+++ lineage_ossi.mk
@@ -0,0 +1,32 @@
+###************************************************************************##
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+###************************************************************************##
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from ossi device
+$(call inherit-product, device/oplus/ossi/device.mk)
+
+PRODUCT_DEVICE := ossi
+PRODUCT_NAME := lineage_ossi
+PRODUCT_BRAND := oplus
+PRODUCT_MODEL := RMX2155
+PRODUCT_MANUFACTURER := oplus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2155L1 \
+    PRODUCT_NAME=RMX2155 \
+    PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1668670161293 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX2155/RMX2155L1:12/SP1A.210812.016/Q.GDPR.bf75e7-1:user/release-keys
+
